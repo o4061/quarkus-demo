8a06bc0e0fa8942f1e864729c83b1d20855a8e3e
userf <userfaltakas@gmail.com>
2022-11-09 14:21:12 +0200

    + time out fallback

--- src/main/kotlin/org/userf/series/TvSeriesResource.kt
+++ src/main/kotlin/org/userf/series/TvSeriesResource.kt
@@ -1,10 +1,12 @@
 package org.userf.series
 
 import org.eclipse.microprofile.faulttolerance.Fallback
+import org.eclipse.microprofile.faulttolerance.Timeout
 import org.eclipse.microprofile.rest.client.inject.RestClient
 import org.userf.series.model.TvSerie
 import org.userf.series.proxy.EpisodesProxy
 import org.userf.series.proxy.TvSeriesProxy
+import java.util.concurrent.TimeUnit
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -35,5 +37,19 @@ class TvSeriesResource {
 
     private fun fallbackGet(title: String): Response {
         return Response.ok(ArrayList<Any>()).build()
+    }
+
+    @Path("timeout")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Fallback(fallbackMethod = "fallbackTimeOut")
+    @Timeout(2000)
+    fun fakeTimeOut(): String {
+        TimeUnit.SECONDS.sleep(3)
+        return "OK"
+    }
+
+    private fun fallbackTimeOut(): String{
+        return "time out"
     }
 }
